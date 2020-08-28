03a7fb7d659d0eb9907e44a2bfeec38f142ea2ec
LagmeraArgus <61847530+LagmeraArgus@users.noreply.github.com>
2020-08-28 18:23:55 -0400

    Adding image
    Adding background image to content cards

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,6 +5,7 @@
     {
       'anchor': 'hero',
       'title': 'Cold Throne Steel',
+      'image': 'assets/exterior-shop-6.png',
       'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }
   ];
@@ -12,7 +13,7 @@
   Vue.component('content-card', {
     props: ['anchor', 'title', 'body'],
     template: `
-    <section class="content-card" v-bind:id="anchor">
+    <section class="content-card" v-bind:id="anchor" v-bind:style="background-image: url(image)">
     <h1 class="content-card__title">{{ title }}</h1>
     <p class="content-card__body">{{ body }}</p>
     </section>`
