941bda0c687d39f86ef75d094abe896421d5ee02
LagmeraArgus <61847530+LagmeraArgus@users.noreply.github.com>
2020-08-29 00:42:12 -0400

    Adding more cards and anchor links

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,13 +5,39 @@
     {
       'anchor': 'hero',
       'title': 'Cold Throne Steel',
+      'image': 'assets/exterior-shop-6.png',
+      'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+      'anchor': 'wares',
+      'title': 'Fine Ishgardian craftsmanship',
       'image': 'assets/kathrine-displaying-the-goods.png',
+      'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+      'anchor': 'services',
+      'title': 'Peerless Service',
+      'image': 'assets/kathrine-at-work.png',
       'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }
   ];
 
+  Vue.component('anchor-link', {
+    props: ['anchor'],
+    template: `
+      <li class="anchor-link"><a v-bind:href="#anchor">{{ anchor }}</a></li>
+    `
+  });
+
   Vue.component('content-card', {
-    props: ['anchor', 'title', 'image', 'body'],
+    props: ['anchor', 'title', 'image', 'body', 'cards'],
+    computed: {
+      activeAnchors = function() {
+        return cards.filter(function(card) {
+          return card.anchor !== anchor;
+        });
+      }
+    },
     template: `
     <section class="content-card" v-bind:id="anchor">
     <div class="content-card__content-wrapper">
@@ -21,6 +47,9 @@
     </div>
     <img class="content-card__image" v-bind:src="image">
     </div>
+    <ul class="content-card__anchors">
+      <anchor-link v-for="anchor-link in active-anchors" v-bind:anchor="anchor-link.anchor"></anchor-link>
+    </ul>
     </section>`
   });
 
