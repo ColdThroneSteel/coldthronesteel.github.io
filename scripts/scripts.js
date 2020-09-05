b9f0fe7dbdc5aea0024c56e16fc43753ff57d258
ColdThroneSteel <70278094+ColdThroneSteel@users.noreply.github.com>
2020-09-05 00:06:34 -0400

    Adding Navigation Bar
    Added a nav bar with links to other tabs, no real functionality yet.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,20 +22,100 @@
     }
   ];
 
-  const anchors = [
+  const navTabs = [
     {
-      'href': 'hero',
-      'title': 'The Shop'
+      'title': 'IC',
+      'id': '#ic',
+      'links': [
+        {
+          'href': '#hero',
+          'title': 'The Shop'
+        },
+        {
+          'href': '#wares',
+          'title': 'Wares'
+        },
+        {
+          'href': '#services',
+          'title': 'Services'
+        }
+      ]
     },
     {
-      'href': 'wares',
-      'title': 'Wares'
+      'title': 'OOC',
+      'id': '#ooc',
+      'links': [
+        {
+          'href': '#hero',
+          'title': 'The Shop'
+        },
+        {
+          'href': '#wares',
+          'title': 'Wares'
+        },
+        {
+          'href': '#services',
+          'title': 'Services'
+        }
+      ]
     },
     {
-      'href': 'services',
-      'title': 'Services'
+      'title': 'Characters',
+      'id': '#characters',
+      'links': [
+        {
+          'href': '#kathrine',
+          'title': 'Kathrine Throne'
+        },
+        {
+          'href': '#bardon',
+          'title': 'Bardon Steele'
+        },
+        {
+          'href': '#lagmera',
+          'title': 'Lagmera Argus'
+        }
+      ]
     }
-  ]
+  ];
+  
+  Vue.component('nav-bar', {
+    props: ['tabs'],
+    methods: {
+      isFirst(index) {
+        return (index === 0? true : false);
+      }
+    },
+    template: `
+    <nav class="nav-bar">
+    <ul class="nav-bar__tab-links">
+    <li class="nav-bar__tab-link" v-for="(tab, index) in tabs"><a v-bind:href="tab.id" v-bind:disabled="isFirst(index)" v-bind:class="{ 'active': isFirst(index), 'nav-bar__link': true }">{{ tab.title }}</a></li>
+    </ul>
+    <section class="nav-bar__tabs">
+    <slot></slot>
+    </section>
+    </nav>
+    `
+  });
+  
+  Vue.component('nav-tab', {
+    props: ['tab', 'index'],
+    template: `
+    <div class="nav-tab" v-bind:class="{ 'active': index === 0 }">
+    <h2 class="nav-tab__title">{{ tab.title }}</h2>
+    <ul class="nav-tab__links">
+    <slot></slot>
+    </ul>
+    </div>
+    `
+  });
+  
+  Vue.component('nav-link', {
+    props: ['link'],
+    template: `
+    <li class="nav-link"><a v-bind:href="link.href" class="nav-bar__link">{{ link.title }}</a></li>
+    `
+  });
 
   Vue.component('content-card', {
     props: ['anchor', 'title', 'image', 'body'],
@@ -53,9 +133,10 @@
 
   window.addEventListener('DOMContentLoaded', (event) => {
     const root = new Vue({
-      el: "#main-content",
+      el: "#shop-app",
       data: {
-        cards: contentCards
+        cards: contentCards,
+        tabs: navTabs
       }
     });
   });
